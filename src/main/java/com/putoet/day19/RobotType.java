e345be0f94d1f4e29c16fae1d271726d47c37b6a
Rene van Putten <z669016@gmail.com>
2022-12-21 06:58:52 +0100

    Day 19 attempt

---
+++ src/main/java/com/putoet/day19/RobotType.java
@@ -0,0 +1,17 @@
+package com.putoet.day19;
+
+public enum RobotType {
+    ORE_ROBOT,
+    CLAY_ROBOT,
+    OBSIDIAN_ROBOT,
+    GEODE_ROBOT;
+
+    public int key() {
+        return switch (this) {
+            case ORE_ROBOT -> 0;
+            case CLAY_ROBOT -> 1;
+            case OBSIDIAN_ROBOT -> 2;
+            case GEODE_ROBOT -> 3;
+        };
+    }
+}
---
+++ src/main/java/com/putoet/day19/Robots.java
@@ -0,0 +1,14 @@
+package com.putoet.day19;
+
+public record Robots(int ore, int clay, int obsidian, int geode) {
+    public static final Robots init = new Robots(1, 0, 0, 0);
+
+    public Robots add(RobotType type) {
+        return switch (type) {
+            case ORE_ROBOT -> new Robots(ore + 1, clay, obsidian, geode);
+            case CLAY_ROBOT -> new Robots(ore, clay + 1, obsidian, geode);
+            case OBSIDIAN_ROBOT -> new Robots(ore, clay, obsidian + 1, geode);
+            case GEODE_ROBOT -> new Robots(ore, clay, obsidian, geode + 1);
+        };
+    }
+}
