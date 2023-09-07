706cf02b5de7d66fe626d25e934aa6cb89762394
Rene van Putten <z669016@gmail.com>
2023-09-07 14:59:42 +0200

    Added Timer, removed dependency on Day base class, reduced visibility scope where possible

--- src/test/java/com/putoet/day17/Day17Test.java
+++ src/test/java/com/putoet/day17/Day17Test.java
@@ -1,7 +1,6 @@
 package com.putoet.day17;
 
 import com.putoet.resources.ResourceLines;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
